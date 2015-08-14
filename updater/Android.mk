CyanogenMod/android_bootable_recovery

updater: Add LZMA support to blockimg
 * This adds support for LZMA compressed blockimages. The space savings
   from doing this is significant and will reduce our CDN costs by quite
   a bit.
 * I'll spend the saved $$$ on beer.

Change-Id: I6679220aaa29592fe6bbccd4136f0c239e45e2ae

updater/Android.mk
@@ -43,7 +43,7 @@ endif
 
 LOCAL_STATIC_LIBRARIES += $(TARGET_RECOVERY_UPDATER_LIBS) $(TARGET_RECOVERY_UPDATER_EXTRA_LIBS)
 LOCAL_STATIC_LIBRARIES += libapplypatch libedify libmtdutils libminzip libz
-LOCAL_STATIC_LIBRARIES += libmincrypt libbz
+LOCAL_STATIC_LIBRARIES += libmincrypt libbz libxz
 LOCAL_STATIC_LIBRARIES += libcutils liblog libstdc++ libc
 LOCAL_STATIC_LIBRARIES += libselinux
 tune2fs_static_libraries := \

NamelessRom/android_bootable_recovery-twrp

updater/Android.mk
@@ -46,7 +46,7 @@ endif
 LOCAL_STATIC_LIBRARIES += $(TARGET_RECOVERY_UPDATER_LIBS) $(TARGET_RECOVERY_UPDATER_EXTRA_LIBS)
 LOCAL_STATIC_LIBRARIES += libapplypatch libedify libmtdutils libminzip libz
 LOCAL_STATIC_LIBRARIES += libflashutils libmmcutils libbmlutils
-LOCAL_STATIC_LIBRARIES += libmincrypttwrp libbz
+LOCAL_STATIC_LIBRARIES += libmincrypttwrp libbz libxz
 LOCAL_STATIC_LIBRARIES += libcutils liblog libstdc++ libc
 LOCAL_STATIC_LIBRARIES += libselinux
 tune2fs_static_libraries := \