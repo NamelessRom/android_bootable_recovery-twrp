020781fa11a61533a9ee5b7acadd58c7542b3a11
Alexander Martinz <eviscerationls@gmail.com>
2015-08-16 19:35:24 +0200

    minzip: fix selinux inclusion
    Change-Id: Iafa4ddafe97f273aeaf8b90909cabd77f2e0ab3b
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- minzip/Android.mk
+++ minzip/Android.mk
@@ -37,15 +37,15 @@ LOCAL_C_INCLUDES += \
 	external/zlib \
 	external/safe-iop/include
 
+LOCAL_MODULE := libminzip
+
+LOCAL_CFLAGS += -Wall
+LOCAL_STATIC_LIBRARIES := libz
+
 ifeq ($(TWHAVE_SELINUX),true)
 LOCAL_C_INCLUDES += external/libselinux/include
 LOCAL_STATIC_LIBRARIES += libselinux
 LOCAL_CFLAGS += -DHAVE_SELINUX
 endif
-
-LOCAL_MODULE := libminzip
-
-LOCAL_CFLAGS += -Wall
-LOCAL_STATIC_LIBRARIES := libz
 
 include $(BUILD_STATIC_LIBRARY)
