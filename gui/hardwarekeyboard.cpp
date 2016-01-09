0c01cf3840a4e14127890ab83e23faf30814b0df
Matt Mower <mowerm@gmail.com>
2016-01-09 14:35:34 -0600

    HardwareKeyboard: Fix event logging
    Change-Id: I75a44d807a17aea6027830d58c37ac130fee12a7

--- gui/hardwarekeyboard.cpp
+++ gui/hardwarekeyboard.cpp
@@ -22,6 +22,7 @@
 #include <set>
 #include <stdbool.h>
 
+#include "../twcommon.h"
 #include "objects.hpp"
 #include "pages.hpp"
 
@@ -346,7 +347,7 @@ static int KeyCodeToChar(int key_code, bool shiftkey, bool ctrlkey)
 
 #ifdef _EVENT_LOGGING
 		default:
-			LOGE("Unmapped keycode: %i\n", key_code);
+			LOGERR("Unmapped keycode: %i\n", key_code);
 			break;
 #endif
 	}
@@ -369,7 +370,7 @@ bool HardwareKeyboard::IsKeyDown(int key_code)
 int HardwareKeyboard::KeyDown(int key_code)
 {
 #ifdef _EVENT_LOGGING
-	LOGE("HardwareKeyboard::KeyDown %i\n", key_code);
+	LOGERR("HardwareKeyboard::KeyDown %i\n", key_code);
 #endif
 	key_code = TranslateKeyCode(key_code);
 	mPressedKeys.insert(key_code);
@@ -395,7 +396,7 @@ int HardwareKeyboard::KeyDown(int key_code)
 int HardwareKeyboard::KeyUp(int key_code)
 {
 #ifdef _EVENT_LOGGING
-	LOGE("HardwareKeyboard::KeyUp %i\n", key_code);
+	LOGERR("HardwareKeyboard::KeyUp %i\n", key_code);
 #endif
 	key_code = TranslateKeyCode(key_code);
 	std::set<int>::iterator itr = mPressedKeys.find(key_code);
@@ -409,7 +410,7 @@ int HardwareKeyboard::KeyUp(int key_code)
 int HardwareKeyboard::KeyRepeat()
 {
 #ifdef _EVENT_LOGGING
-	LOGE("HardwareKeyboard::KeyRepeat: %i\n", mLastKeyChar);
+	LOGERR("HardwareKeyboard::KeyRepeat: %i\n", mLastKeyChar);
 #endif
 	if (mLastKeyChar)
 		PageManager::NotifyCharInput(mLastKeyChar);
