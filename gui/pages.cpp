c2b307e3a6f4c69f32906a8f130b286d80b6dbb1
Ethan Yonker <dees_troy@teamw.in>
2016-01-22 16:07:11 -0600

    Fix (Auto Generate) handling for backup names
    Change-Id: I29f3823d03c005fb55a0e0a98818d40bd1eb039d

--- gui/pages.cpp
+++ gui/pages.cpp
@@ -684,6 +684,7 @@ int PageSet::LoadLanguage(char* languageFile, ZipArchive* package)
 	xml_node<>* parent;
 	xml_node<>* child;
 	std::string resource_source;
+	int ret = 0;
 
 	if (languageFile) {
 		LOGINFO("Parsing languageFile...");
@@ -714,9 +715,10 @@ int PageSet::LoadLanguage(char* languageFile, ZipArchive* package)
 	if (child)
 		mResources->LoadResources(child, package, resource_source);
 	else
-		return -1;
+		ret = -1;
+	DataManager::SetValue("tw_backup_name", gui_lookup("auto_generate", "(Auto Generate)"));
 	lang.clear();
-	return 0;
+	return ret;
 }
 
 int PageSet::Load(ZipArchive* package, char* xmlFile, char* languageFile, char* baseLanguageFile)
